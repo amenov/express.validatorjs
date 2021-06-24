68557ed38b9ba81ae51f1bf67dbf4bd21b441435
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2021-06-24 13:47:57 +0600

    first commit

---
+++ middleware.js
@@ -0,0 +1,13 @@
+const Validator = require("amenov.validator");
+
+module.exports = (options) => (req, res, next) => {
+  req.validation = async (rules, target = "body") => {
+    const validation = new Validator(req[target], rules, options);
+
+    await validation.fails();
+
+    return validation.failed ? validation.errors : null;
+  };
+
+  next();
+};
